5272b3b1438604f34114a64803cf88011d368587
Denis5201 <builukden@yandex.ru>
2024-03-18 18:31:49 +0700

    Добавил обработку ошибок

---
+++ src/DP-backend.Domain.Identity/Exceptions/BadDataException.cs
@@ -0,0 +1,12 @@
+﻿namespace DP_backend.Models.Exceptions
+{
+    [Serializable]
+    public class BadDataException : Exception
+    {
+        public BadDataException() { }
+
+        public BadDataException(string message) : base(message) { }
+
+        public BadDataException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
