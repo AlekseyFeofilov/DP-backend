3c4d9e19b3cd43f2771465a6b57b59c4159d81d4
Denis5201 <builukden@yandex.ru>
2024-06-16 05:50:32 +0700

    Добавил уведомления

---
+++ src/DP-backend/Models/DTOs/NotificationDTO.cs
@@ -0,0 +1,34 @@
+﻿using DP_backend.Domain.Employment;
+
+namespace DP_backend.Models.DTOs
+{
+    public class NotificationDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public Guid AddresseeId { get; set; }
+
+        public string Link { get; set; }
+
+        public NotificationType Type { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        public NotificationDTO() { }
+
+        public NotificationDTO(Notification notification)
+        {
+            Id = notification.Id;
+            Title = notification.Title;
+            Message = notification.Message;
+            AddresseeId = notification.AddresseeId;
+            Link = notification.Link;
+            Type = notification.Type;
+            CreateDateTime = notification.CreateDateTime;
+        }
+    }
+}
